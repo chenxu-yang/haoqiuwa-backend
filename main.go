7cba2601b4dc1712c77a796b0d5da258b8cfdb26
cianoyang <cianoyang@tencent.com>
2023-06-19 20:20:20 +0800

    init time

--- main.go
+++ main.go
@@ -11,8 +11,17 @@ import (
 )
 
 func main() {
-	var cstZone = time.FixedZone("CST", 8*3600) // 东八
-	time.Local = cstZone
+	// 加载指定时区的位置信息
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// 设置全局默认时区
+	time.Local = loc
+	// 获取当前时间
+	now := time.Now()
+	fmt.Println(now)
 	if err := db.Init(); err != nil {
 		panic(fmt.Sprintf("mysql init failed with %+v", err))
 	}
